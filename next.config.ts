c643db290c06026c8b6148bab0ec319330f48016
Hunter Medney <hmedney@gmail.com>
2025-08-11 06:55:43 -0400

    suppress deprecation warnings

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
 import type {NextConfig} from 'next';
-import path from 'node:path';
 
 const nextConfig: NextConfig = {
-  // sassOptions: {
-  //   loadPaths: [path.resolve(__dirname, 'node_modules')]
-  // },
-  // turbopack: {
-  //   resolveAlias: {
-  //     '~@ibm/plex': path.resolve(__dirname, 'node_modules/@ibm/plex')
-  //   }
-  // }
+  // https://sass-lang.com/documentation/breaking-changes/mixed-decls/#silencing-deprecations-in-dependencies
+  sassOptions: {
+    quietDeps: true
+  }
 };
 
 export default nextConfig;
