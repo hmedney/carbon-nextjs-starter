b966f36d327e677d4f06b0a2f790754101d067ae
Hunter Medney <hmedney@gmail.com>
2025-08-11 06:55:37 -0400

    add title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
+import {Metadata} from 'next';
 import './globals.scss';
+
+export const metadata: Metadata = {
+  title: 'Carbon NextJs starter'
+};
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
